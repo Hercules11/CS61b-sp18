a537c46145eb18eec2501582622e1b587c2b55ca
WangXX-Orange <428230171@qq.com>
2020-11-25 11:45:51 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
+/*
+*  1. All code in Java is a part of a class
+*  2. All statements in Java must end with semi-colons */
