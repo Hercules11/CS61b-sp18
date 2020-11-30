828bb969841b31ee0f3ac9dd39ef2beb191a4c48
WangXX-Orange <428230171@qq.com>
2020-11-30 13:14:10 +0800

    complete prog1b

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,4 +1,4 @@
-public class Palindrome extends ArrayDeque<Character> {
+public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
         ArrayDeque<Character> stringDeque = new ArrayDeque<>();
         for (int i = 0; i < word.length(); i++) {
@@ -7,7 +7,7 @@ public class Palindrome extends ArrayDeque<Character> {
         return stringDeque;
     }
 
-    public boolean isPalindrome(String word) {
+    boolean isPalindrome(String word) {
         if (word.length() == 0 || word.length() == 1) {
             return true;
         } else if (word.charAt(0) != word.charAt(word.length() - 1)) {
@@ -18,7 +18,7 @@ public class Palindrome extends ArrayDeque<Character> {
 
     }
 
-    public boolean isPalindrome(String word, CharacterComparator cc) {
+    boolean isPalindrome(String word, CharacterComparator cc) {
         if (word.length() == 0 || word.length() == 1) {
             return true;
         } else if (!cc.equalChars(word.charAt(0), word.charAt(word.length() - 1))) {
@@ -28,7 +28,7 @@ public class Palindrome extends ArrayDeque<Character> {
         }
     }
 
-    public boolean isPalindrome(String word, OffByN cc) {
+    boolean isPalindrome(String word, OffByN cc) {
         if (word.length() == 0 || word.length() == 1) {
             return true;
         } else if (!cc.equalChars(word.charAt(0), word.charAt(word.length() - 1))) {
