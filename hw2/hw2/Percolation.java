35fcbcb8039abf6f8c90f1fec19cc17482b26ca9
WangXX-Orange <428230171@qq.com>
2020-12-06 13:47:42 +0800

    complete hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,7 +3,7 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int[][] perc; // 值为1 则表示open, 和第一行open的格子connected 则表示full
+    private int[][] perc; // 值为1,表示open, 和第一行open的格子connected,表示full
     private WeightedQuickUnionUF WQUDS;
     private int numberOpen = 0;
     private int size;
@@ -15,9 +15,7 @@ public class Percolation {
         size = N;
         perc = new int[N][N];
         WQUDS = new WeightedQuickUnionUF(N * N);
-        // initialize a DS that size is N * N
     }
-    // create N-by-N grid, with all sites initially blocked
 
     private void findOpen(int row, int col) {
         int upRow = row - 1;
@@ -37,7 +35,6 @@ public class Percolation {
             WQUDS.union(row * size + col, row * size + col + 1);
         }
     }
-    // find the surrounding open sites
 
     public void open(int row, int col) {
         if (row < 0 || col < 0 || col >= size || row >= size) {
@@ -49,7 +46,6 @@ public class Percolation {
             findOpen(row, col);
         }
     }
-    // open the site (row, col) if it is not open already
 
     public boolean isOpen(int row, int col) {
         if (row < 0 || col < 0 || col >= size || row >= size) {
@@ -57,7 +53,6 @@ public class Percolation {
         }
         return perc[row][col] == 1;
     }
-    // is the site (row, col) open?
 
     public boolean isFull(int row, int col) {
         if (row < 0 || col < 0 || col >= size || row >= size) {
@@ -72,12 +67,10 @@ public class Percolation {
         }
         return false;
     }
-    // is the site (row, col) full?
 
     public int numberOfOpenSites() {
         return numberOpen;
     }
-    // number of open sites
 
     public boolean percolates() {
         for (int i = 0; i < size; i++) {
@@ -93,23 +86,11 @@ public class Percolation {
         }
         return false;
     }
-    // does the system percolate?
 
     public static void main(String[] args) {
         Percolation perc = new Percolation(1);
-//        perc.open(0, 3);
-//        perc.open(1, 3);
-//        perc.open(2, 3);
-//        perc.open(3, 3);
-//        perc.open(4, 3);
         System.out.println("if the system percolate: " + perc.percolates());
         System.out.println("The number of open sites: " + perc.numberOfOpenSites());
-//        System.out.println("if (2, 3) is full: " + perc.isFull(2, 4));
-//        System.out.println("if (1, 3) is full: " + perc.isFull(1, 3));
-//        System.out.println("if (0, 3) is full: " + perc.isFull(0, 3));
-//        System.out.println("if (3, 4) is open: " + perc.isOpen(3, 4));
-//        perc.open(3, 4);
-//        System.out.println("if (3, 4) is open: " + perc.isOpen(3, 4));
     }
     // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -5,8 +5,6 @@ import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
     private double[] numberPerc;
-//    private double average;
-//    private double stddiff;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
@@ -27,35 +25,27 @@ public class PercolationStats {
             double frac = count / (N * N);
             numberPerc[i] = frac;
         }
-    }   // perform T independent experiments on an N-by-N grid
+    }
 
     public double mean() {
         return StdStats.mean(numberPerc);
-    }   // sample mean of percolation threshold
+    }
 
     public double stddev() {
         return StdStats.stddev(numberPerc);
-    }   // sample standard deviation of percolation threshold
+    }
 
     public double confidenceLow() {
         return mean() - 1.96 * stddev() / Math.sqrt(numberPerc.length);
-    }   // low endpoint of 95% confidence interval
+    }
 
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(numberPerc.length);
-    }   // high endpoint of 95% confidence interval
+    }
 
 //    public static void main(String[] args) {
 //        PercolationFactory pf = new PercolationFactory();
 //        PercolationStats percolation = new PercolationStats(20, 10, pf);
 //        System.out.println("THe stddev of system is: " + percolation.stddev());
-//        System.out.println("THe high of system is: " + percolation.confidenceHigh());
-//        System.out.println("THe high of system is: " + percolation.confidenceHigh());
-//        System.out.println("THe stddev of system is: " + percolation.stddev());
-//        System.out.println("THe low of system is: " + percolation.confidenceLow());
-//        System.out.println("THe mean of system is: " + percolation.mean());
-//        System.out.println("THe low of system is: " + percolation.confidenceLow());
-//        System.out.println("THe high of system is: " + percolation.confidenceHigh());
 //    }
-
 }
